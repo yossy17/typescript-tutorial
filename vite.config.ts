8e0b3474ce3beceaefe8ee33e23e66ae739206c8
Yos_sy <hiitoy0117@gmail.com>
2024-08-04 07:23:53 +0900

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // Viteの設定オプション
-  root: "src",
-  build: {
-    outDir: "dist",
+  optimizeDeps: {
+    include: ["highlight.js/lib/core"],
   },
 });
